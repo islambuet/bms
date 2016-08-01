755c1bfad1bba7908145d49c363a147c77c85d79
islambuet <islambuet@gmail.com>
2016-08-01 20:45:21 +0600

    pricing automated completed

--- application/controllers/Mgt_sales_pricing_automated.php
+++ application/controllers/Mgt_sales_pricing_automated.php
@@ -195,6 +195,26 @@ class Mgt_sales_pricing_automated extends Root_Controller
         $this->db->order_by('v.ordering','ASC');
 
         $results=$this->db->get()->result_array();
+        $grand_row=$crop_row=$type_row=array();
+        $grand_row['crop_name']=$crop_row['crop_name']=$type_row['crop_name']='';
+        $grand_row['crop_type_name']=$crop_row['crop_type_name']=$type_row['crop_type_name']='';
+        $grand_row['variety_name']=$crop_row['variety_name']=$type_row['variety_name']='';
+        $type_row['variety_name']='Total Type';
+        $crop_row['crop_type_name']='Total Crop';
+        $grand_row['crop_name']='Grand Total';
+        $grand_row['hom_target']=$crop_row['hom_target']=$type_row['hom_target']='0';
+        $grand_row['cogs']=$crop_row['cogs']=$type_row['cogs']='0';
+        $grand_row['general']=$crop_row['general']=$type_row['general']='0';
+        $grand_row['marketing']=$crop_row['marketing']=$type_row['marketing']='0';
+        $grand_row['finance']=$crop_row['finance']=$type_row['finance']='0';
+        $grand_row['profit']=$crop_row['profit']=$type_row['profit']='0';
+        $grand_row['net_price']=$crop_row['net_price']=$type_row['net_price']='0';
+        $grand_row['sales_commission']=$crop_row['sales_commission']=$type_row['sales_commission']='0';
+        $grand_row['incentive']=$crop_row['incentive']=$type_row['incentive']='0';
+        $grand_row['trade_price']=$crop_row['trade_price']=$type_row['trade_price']='0';
+        $grand_row['total_profit']=$crop_row['total_profit']=$type_row['total_profit']='0';
+        $grand_row['total_net_price']=$crop_row['total_net_price']=$type_row['total_net_price']='0';
+        $grand_row['profit_percentage']=$crop_row['profit_percentage']=$type_row['profit_percentage']='0';
 
         $prev_crop_name='';
         $prev_crop_type_name='';
@@ -205,22 +225,16 @@ class Mgt_sales_pricing_automated extends Root_Controller
             {
                 if($prev_crop_name!=$result['crop_name'])
                 {
-                    //adding total type column
-                    /*$type_row=array();
-                    $type_row['crop_name']='';
-                    $type_row['crop_type_name']='';
-                    $type_row['variety_name']='Total Type';
-                    $type_row['hom_target']='0';
-                    $type_row['cogs']='0';
                     $items[]=$this->get_report_row($type_row);
-                    //adding total crop column
-                    $crop_row=array();
-                    $crop_row['crop_name']='';
-                    $crop_row['crop_type_name']='Total Crop';
-                    $crop_row['variety_name']='';
+                    $type_row['hom_target']=0;
+                    $type_row['total_profit']=0;
+                    $type_row['total_net_price']=0;
+
+                    $items[]=$this->get_report_row($crop_row);
                     $crop_row['hom_target']=0;
-                    $crop_row['cogs']=0;
-                    $items[]=$this->get_report_row($crop_row);*/
+                    $crop_row['total_profit']=0;
+                    $crop_row['total_net_price']=0;
+
                     $item['crop_name']=$result['crop_name'];
                     $prev_crop_name=$result['crop_name'];
 
@@ -229,12 +243,10 @@ class Mgt_sales_pricing_automated extends Root_Controller
                 }
                 elseif($prev_crop_type_name!=$result['crop_type_name'])
                 {
-                    //adding total column resetting types
-                    /*$type_row=array();
-                    $type_row['crop_name']='';
-                    $type_row['crop_type_name']='';
-                    $type_row['variety_name']='Total Type';
-                    $items[]=$type_row;*/
+                    $items[]=$this->get_report_row($type_row);
+                    $type_row['hom_target']=0;
+                    $type_row['total_profit']=0;
+                    $type_row['total_net_price']=0;
 
                     $item['crop_name']='';
                     $item['crop_type_name']=$result['crop_type_name'];
@@ -278,11 +290,11 @@ class Mgt_sales_pricing_automated extends Root_Controller
                 $item['marketing']=$item['cogs']*$indirect_costs['marketing']/100;
                 $item['finance']=$item['cogs']*$indirect_costs['finance']/100;
                 $item['profit']=$item['cogs']*$indirect_costs['profit']/100;
+
                 $item['net_price']=$item['cogs']+$item['general']+$item['marketing']+$item['finance']+$item['profit'];
                 $item['sales_commission']=$item['net_price']*$indirect_costs['sales_commission']/100;
                 $item['incentive']=$item['net_price']*$indirect_costs['incentive']/100;
                 $item['trade_price']=$item['net_price']+$item['sales_commission']+$item['incentive'];
-                $item['profit_percentage']=$indirect_costs['profit'];
             }
             else
             {
@@ -294,11 +306,47 @@ class Mgt_sales_pricing_automated extends Root_Controller
                 $item['sales_commission']=0;
                 $item['incentive']=0;
                 $item['trade_price']=$item['cogs'];
+            }
+            if(($item['profit']*$item['hom_target'])!=0)
+            {
+                $item['total_profit']=$item['profit']*$item['hom_target'];
+            }
+            else
+            {
+                $item['total_profit']=0;
+            }
+            if(($item['net_price']*$item['hom_target'])!=0)
+            {
+                $item['total_net_price']=$item['net_price']*$item['hom_target'];
+            }
+            else
+            {
+                $item['total_net_price']=0;
+            }
+
+            if($item['net_price']!=0)
+            {
+                $item['profit_percentage']=$item['profit']*100/$item['net_price'];
+            }
+            else
+            {
                 $item['profit_percentage']=0;
             }
+            $type_row['hom_target']+=$item['hom_target'];
+            $crop_row['hom_target']+=$item['hom_target'];
+            $grand_row['hom_target']+=$item['hom_target'];
+            $type_row['total_profit']+=$item['total_profit'];
+            $crop_row['total_profit']+=$item['total_profit'];
+            $grand_row['total_profit']+=$item['total_profit'];
+            $type_row['total_net_price']+=$item['total_net_price'];
+            $crop_row['total_net_price']+=$item['total_net_price'];
+            $grand_row['total_net_price']+=$item['total_net_price'];
 
             $items[]=$this->get_report_row($item);
         }
+        $items[]=$this->get_report_row($type_row);
+        $items[]=$this->get_report_row($crop_row);
+        $items[]=$this->get_report_row($grand_row);
 
         $this->jsonReturn($items);
     }
@@ -388,25 +436,41 @@ class Mgt_sales_pricing_automated extends Root_Controller
         {
             $row['trade_price']='';
         }
-        if(($item['profit']*$item['hom_target'])!=0)
+        if($item['total_profit']!=0)
+        {
+            $row['total_profit']=number_format($item['total_profit'],2);
+        }
+        else
+        {
+            $row['total_profit']='';
+        }
+        if($item['total_net_price']!=0)
+        {
+            $row['total_net_price']=number_format($item['total_net_price'],2);
+        }
+        else
+        {
+            $row['total_net_price']='';
+        }
+        /*if(($item['profit']*$item['hom_target'])!=0)
         {
             $row['total_profit']=number_format($item['profit']*$item['hom_target'],2);
         }
         else
         {
             $row['total_profit']='';
-        }
-        if(($item['net_price']*$item['hom_target'])!=0)
+        }*/
+        /*if(($item['net_price']*$item['hom_target'])!=0)
         {
             $row['total_net_price']=number_format($item['net_price']*$item['hom_target'],2);
         }
         else
         {
             $row['total_net_price']='';
-        }
+        }*/
         if($item['profit_percentage']!=0)
         {
-            $row['profit_percentage']=$item['profit_percentage'];
+            $row['profit_percentage']=number_format($item['profit_percentage'],2);
         }
         else
         {
