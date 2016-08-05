4bfad73a183adec0977692d0c6f4ccebc4324b25
islambuet <islambuet@gmail.com>
2016-08-05 23:33:42 +0600

    hom sales pricing started

---
+++ application/controllers/Hom_sales_pricing.php
@@ -0,0 +1,321 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Hom_sales_pricing extends Root_Controller
+{
+    private  $message;
+    public $permissions;
+    public $controller_url;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->message="";
+        $this->permissions=User_helper::get_permission('Hom_sales_pricing');
+        $this->controller_url='hom_sales_pricing';
+
+    }
+
+    public function index($action="search",$id1=0,$id2=0,$id3=0)
+    {
+        if($action=="search")
+        {
+            $this->system_search();
+        }
+        elseif($action=="edit")
+        {
+            $this->system_edit();
+        }
+        elseif($action=="get_edit_items")
+        {
+            $this->system_get_edit_items();
+        }
+        elseif($action=="save")
+        {
+            $this->system_save();
+        }
+        else
+        {
+            $this->system_search();
+        }
+    }
+    private function system_search()
+    {
+        if(isset($this->permissions['view'])&&($this->permissions['view']==1))
+        {
+
+            $fy_info=System_helper::get_fiscal_years();
+            $data['years']=$fy_info['years'];
+            $data['crops']=Query_helper::get_info($this->config->item('ems_setup_classification_crops'),array('id value','name text'),array(),0,0,array('ordering ASC'));
+            $data['title']="Pricing Marketing Search";
+            $ajax['status']=true;
+            $ajax['system_content'][]=array("id"=>"#system_content","html"=>$this->load->view("hom_sales_pricing/search",$data,true));
+            if($this->message)
+            {
+                $ajax['system_message']=$this->message;
+            }
+            $ajax['system_page_url']=site_url($this->controller_url);
+            $this->jsonReturn($ajax);
+        }
+        else
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->jsonReturn($ajax);
+        }
+    }
+
+    private function system_edit()
+    {
+        if(isset($this->permissions['edit'])&&($this->permissions['edit']==1))
+        {
+            $year0_id=$this->input->post('year0_id');
+            $crop_id=$this->input->post('crop_id');
+
+            $crop=Query_helper::get_info($this->config->item('ems_setup_classification_crops'),array('id value','name text'),array('id ='.$crop_id),1);
+            $data['years']=Query_helper::get_info($this->config->item('ems_basic_setup_fiscal_year'),array('id value','name text'),array('status ="'.$this->config->item('system_status_active').'"',' id >='.$year0_id),$this->config->item('num_year_prediction')+1,0,array('id ASC'));
+            $data['year0_id']=$year0_id;
+            $data['crop_id']=$crop_id;
+            $keys=',';
+            $keys.="year0_id:'".$year0_id."',";
+            $keys.="crop_id:'".$crop_id."',";
+            $data['keys']=trim($keys,',');
+
+
+            $data['title']="Pricing Marketing For ".$crop['text'].'('.$data['years'][0]['text'].')';
+
+            $ajax['status']=true;
+            $ajax['system_content'][]=array("id"=>"#system_report_container","html"=>$this->load->view("hom_sales_pricing/add_edit",$data,true));
+            if($this->message)
+            {
+                $ajax['system_message']=$this->message;
+            }
+            $this->jsonReturn($ajax);
+        }
+        else
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->jsonReturn($ajax);
+        }
+    }
+    private function system_get_edit_items()
+    {
+        $items=array();
+        $year0_id=$this->input->post('year0_id');
+        $crop_id=$this->input->post('crop_id');
+
+        $this->db->from($this->config->item('table_mgt_sales_pricing').' sp');
+        $this->db->select('sp.*');
+        $this->db->select('v.name variety_name');
+        $this->db->select('type.name type_name');
+        $this->db->join($this->config->item('ems_setup_classification_varieties').' v','v.id = sp.variety_id','INNER');
+        $this->db->join($this->config->item('ems_setup_classification_crop_types').' type','type.id = v.crop_type_id','INNER');
+        $this->db->where('v.whose','ARM');
+        $this->db->where('v.status =',$this->config->item('system_status_active'));
+        $this->db->where('type.crop_id',$crop_id);
+        $this->db->where('sp.year0_id',$year0_id);
+        $this->db->order_by('type.ordering','ASC');
+        $this->db->order_by('v.ordering','ASC');
+
+        $results=$this->db->get()->result_array();
+
+
+        $prev_type='';
+        foreach($results as $index=>$result)
+        {
+            $item=array();
+            if($index>0)
+            {
+                if($prev_type!=$result['type_name'])
+                {
+                    $item['type_name']=$result['type_name'];
+                    $prev_type=$result['type_name'];
+                }
+                else
+                {
+                    $item['type_name']='';
+                }
+            }
+            else
+            {
+                $prev_type=$result['type_name'];
+                $item['type_name']=$result['type_name'];
+            }
+            $item['variety_id']=$result['variety_id'];
+            $item['variety_name']=$result['variety_name'];
+            $item['tp_last_year']=0;
+            $item['tp_management']=$result['tp_management'];
+            if($result['user_created_hom']>0)
+            {
+                $item['tp_hom']=$result['hom'];
+                $item['commission_hom']=$result['commission_hom'];
+                $item['incentive_hom']=$result['incentive_hom'];
+            }
+            else
+            {
+                $item['tp_hom']=0;
+                $item['commission_hom']=$result['commission_management'];
+                $item['incentive_hom']=$result['incentive_management'];
+            }
+            $item['sales_commission']=$item['tp_hom']*$item['commission_hom']/100;
+            $item['incentive']=$item['tp_hom']*$item['incentive_hom']/100;
+            $item['net_price']=$item['tp_hom']-$item['sales_commission']-$item['incentive'];
+
+            $items[]=$this->get_report_row($item);
+
+        }
+
+        $this->jsonReturn($items);
+
+    }
+    private function get_report_row($item)
+    {
+        $row=array();
+        $row['type_name']=$item['type_name'];
+        $row['variety_id']=$item['variety_id'];
+        $row['variety_name']=$item['variety_name'];
+        if($item['tp_last_year']!=0)
+        {
+            $row['tp_last_year']=$item['tp_last_year'];
+        }
+        else
+        {
+            $row['tp_last_year']='';
+        }
+        if($item['tp_management']!=0)
+        {
+            $row['tp_management']=number_format($item['tp_management'],2);
+        }
+        else
+        {
+            $row['tp_management']='';
+        }
+        if($item['tp_hom']!=0)
+        {
+            $row['tp_hom']=$item['tp_hom'];
+        }
+        else
+        {
+            $row['tp_hom']='';
+        }
+        if($item['commission_hom']!=0)
+        {
+            $row['commission_hom']=$item['commission_hom'];
+        }
+        else
+        {
+            $row['commission_hom']='';
+        }
+        if($item['sales_commission']!=0)
+        {
+            $row['sales_commission']=number_format($item['sales_commission'],2);
+        }
+        else
+        {
+            $row['sales_commission']='';
+        }
+        if($item['incentive_hom']!=0)
+        {
+            $row['incentive_hom']=$item['incentive_hom'];
+        }
+        else
+        {
+            $row['incentive_hom']='';
+        }
+        if($item['incentive']!=0)
+        {
+            $row['incentive']=number_format($item['incentive'],2);
+        }
+        else
+        {
+            $row['incentive']='';
+        }
+        if($item['net_price']!=0)
+        {
+            $row['net_price']=number_format($item['net_price'],2);
+        }
+        else
+        {
+            $row['net_price']='';
+        }
+        return $row;
+
+    }
+    private function system_save()
+    {
+        if(isset($this->permissions['edit'])&&($this->permissions['edit']==1))
+        {
+            $year0_id=$this->input->post('year0_id');
+            $crop_id=$this->input->post('crop_id');
+            $user = User_helper::get_user();
+            $time=time();
+
+            $items=$this->input->post('items');
+            $this->db->trans_start();
+            if(sizeof($items)>0)
+            {
+                $sales_pricing=array();
+                $results=Query_helper::get_info($this->config->item('table_mgt_sales_pricing'),'*',array('year0_id ='.$year0_id));
+                foreach($results as $result)
+                {
+                    $sales_pricing[$result['variety_id']]=$result;
+                }
+
+                foreach($items as $variety_id=>$data)
+                {
+                    if(strlen(trim($data['tp_management']))==0)
+                    {
+                        $data['tp_management']=0;
+                    }
+                    if(strlen(trim($data['commission_management']))==0)
+                    {
+                        $data['commission_management']=0;
+                    }
+                    if(strlen(trim($data['incentive_management']))==0)
+                    {
+                        $data['incentive_management']=0;
+                    }
+                    if(isset($sales_pricing[$variety_id]))
+                    {
+                        $data['user_updated'] = $user->user_id;
+                        $data['date_updated'] = $time;
+                        $data['user_update_management'] = $user->user_id;
+                        $data['date_updated_management'] = $time;
+                        Query_helper::update($this->config->item('table_mgt_sales_pricing'),$data,array("id = ".$sales_pricing[$variety_id]['id']));
+                    }
+                    else
+                    {
+                        $data['year0_id']=$year0_id;
+                        $data['variety_id']=$variety_id;
+                        $data['user_created'] = $user->user_id;
+                        $data['date_created'] = $time;
+                        $data['user_created_management'] = $user->user_id;
+                        $data['date_created_management'] = $time;
+                        Query_helper::add($this->config->item('table_mgt_sales_pricing'),$data);
+                    }
+
+                }
+            }
+
+            $this->db->trans_complete();   //DB Transaction Handle END
+            if ($this->db->trans_status() === TRUE)
+            {
+                $this->message=$this->lang->line("MSG_SAVED_SUCCESS");
+                $this->system_search();
+            }
+            else
+            {
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("MSG_SAVED_FAIL");
+                $this->jsonReturn($ajax);
+            }
+        }
+        else
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->jsonReturn($ajax);
+        }
+
+    }
+
+}
---
+++ application/views/hom_sales_pricing/add_edit.php
@@ -0,0 +1,261 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+    $CI = & get_instance();
+    $action_data=array();
+    $action_data["action_save_jqx"]='#save_form_jqx';
+    $CI->load->view("action_buttons",$action_data);
+?>
+<form class="form_valid" id="save_form_jqx" action="<?php echo site_url($CI->controller_url.'/index/save');?>" method="post">
+    <input type="hidden" name="year0_id" value="<?php echo $year0_id; ?>" />
+    <input type="hidden" name="crop_id" value="<?php echo $crop_id; ?>" />
+    <div id="jqx_inputs">
+    </div>
+</form>
+<div class="row widget">
+    <div class="widget-header">
+        <div class="title">
+            <?php echo $title; ?>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <?php
+    if(isset($CI->permissions['column_headers'])&&($CI->permissions['column_headers']==1))
+    {
+
+        ?>
+        <div class="col-xs-12" style="margin-bottom: 20px;">
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="type_name"><?php echo $CI->lang->line('LABEL_CROP_TYPE'); ?></label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="variety_name"><?php echo $CI->lang->line('LABEL_VARIETY_NAME'); ?></label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="hom_target">Target</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="tp_last_year">Last Year TP</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="tp_automated">Automated TP</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="tp_mgt">Management TP</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="sales_commission_percentage">Commission %</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="sales_commission">Commission</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="incentive_percentage">Incentive %</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="incentive">Incentive</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="net_price">Net Price</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="cogs">COGS</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="general">General</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="marketing">Marketing</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="finance">Finance</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="profit">Profit</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="total_net_price">Total Net Price</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="total_profit">Total Profit</label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="profit_percentage">Profit %</label>
+
+        </div>
+        <?php
+    }
+    ?>
+    <div class="col-xs-12" id="system_jqx_container">
+
+    </div>
+</div>
+<div class="clearfix"></div>
+<script type="text/javascript">
+    function calculate_total(row, tp_hom, sales_commission_percentage,incentive_percentage)
+    {
+        var row_data = $('#system_jqx_container').jqxGrid('getrowdata', row);
+
+        var sales_commission=tp_hom*sales_commission_percentage/100;
+        var incentive=tp_hom*incentive_percentage/100;
+
+        if(sales_commission!=0)
+        {
+            $("#system_jqx_container").jqxGrid('setcellvalue', row, 'sales_commission', number_format(sales_commission,2));
+        }
+        else
+        {
+            $("#system_jqx_container").jqxGrid('setcellvalue', row, 'sales_commission', '');
+        }
+        if(incentive!=0)
+        {
+            $("#system_jqx_container").jqxGrid('setcellvalue', row, 'incentive', number_format(incentive,2));
+        }
+        else
+        {
+            $("#system_jqx_container").jqxGrid('setcellvalue', row, 'incentive', '');
+        }
+        var net_price=tp_hom-sales_commission-incentive;
+        if(net_price!=0)
+        {
+            $("#system_jqx_container").jqxGrid('setcellvalue', row, 'net_price', number_format(net_price,2));
+        }
+        else
+        {
+            $("#system_jqx_container").jqxGrid('setcellvalue', row, 'net_price', '');
+        }
+
+
+    }
+    $(document).ready(function ()
+    {
+        var cellsrenderer = function(row, column, value, defaultHtml, columnSettings, record)
+        {
+            var element = $(defaultHtml);
+            // console.log(defaultHtml);
+
+            element.css({'margin': '0px','width': '100%', 'height': '100%',padding:'5px','whiteSpace':'normal'});
+            if(column=='tp_hom' ||column=='commission_hom' ||column=='incentive_hom')
+            {
+                element.html('<div class="jqxgrid_input">'+value+'</div>');
+            }
+
+            return element[0].outerHTML;
+
+        };
+        var tooltiprenderer = function (element) {
+            $(element).jqxTooltip({position: 'mouse', content: $(element).text() });
+        };
+        var aggregates=function (aggregatedValue, currentValue, column, record)
+        {
+            var total=0;
+            var av=String(aggregatedValue);
+            if(!isNaN(parseFloat(av.replace(/,/g,''))))
+            {
+                total=parseFloat(av.replace(/,/g,''));
+            }
+            var cv=String(record[column]);
+            if(!isNaN(parseFloat(cv.replace(/,/g,''))))
+            {
+                total=total+parseFloat(cv.replace(/,/g,''));
+            }
+            if(total!=0)
+            {
+                return number_format(total,2);
+            }
+            else
+            {
+                return '';
+            }
+            //return grand_starting_stock;
+        };
+        var aggregatesrenderer=function (aggregates)
+        {
+            return '<div style="position: relative; margin: 0px;padding: 5px;width: 100%;height: 100%; overflow: hidden;background-color:#AEC2DD;">' +aggregates['total']+'</div>';
+
+        };
+
+        var url = "<?php echo site_url($CI->controller_url."/index/get_edit_items/");?>";
+
+        // prepare the data
+        var source =
+        {
+            dataType: "json",
+            dataFields: [
+                { name: 'id', type: 'int' },
+                { name: 'type_name', type: 'string' },
+                { name: 'variety_id', type: 'string' },
+                { name: 'variety_name', type: 'string' },
+                { name: 'tp_last_year', type: 'string' },
+                { name: 'tp_management', type: 'string' },
+                { name: 'tp_hom', type: 'string' },
+                { name: 'commission_hom', type: 'string' },
+                { name: 'sales_commission', type: 'string' },
+                { name: 'incentive_hom', type: 'string' },
+                { name: 'incentive', type: 'string' },
+                { name: 'net_price', type: 'string' }
+            ],
+            id: 'id',
+            url: url,
+            type: 'POST',
+            data:{<?php echo $keys; ?>}
+        };
+        var dataAdapter = new $.jqx.dataAdapter(source);
+
+        // create jqxgrid.
+        $("#system_jqx_container").jqxGrid(
+            {
+                width: '100%',
+                height:'300',
+                source: dataAdapter,
+                columnsresize: true,
+                columnsreorder: true,
+                altrows: true,
+                rowsheight: 35,
+                showstatusbar: true,
+                showaggregates: true,
+                editable:true,
+                columns: [
+                    { text: '<?php echo $CI->lang->line('LABEL_CROP_TYPE'); ?>', dataField: 'type_name',width: '80',cellsrenderer: cellsrenderer,pinned:true,rendered: tooltiprenderer,editable:false},
+                    { text: '<?php echo $CI->lang->line('LABEL_VARIETY_NAME'); ?>', dataField: 'variety_name',width: '130',cellsrenderer: cellsrenderer,pinned:true,rendered: tooltiprenderer,editable:false},
+                    {text: 'Last Year TP', dataField: 'tp_last_year',align:'center',cellsalign: 'right',width:'110',cellsrenderer: cellsrenderer,rendered: tooltiprenderer,editable:false},
+                    {text: 'Management TP', dataField: 'tp_management',align:'center',cellsalign: 'right',width:'110',cellsrenderer: cellsrenderer,rendered: tooltiprenderer,editable:false},
+                    {text: 'HOM TP', dataField: 'tp_hom',align:'center',cellsalign: 'right',width:'150',cellsrenderer: cellsrenderer,rendered: tooltiprenderer,editable:true,columntype:'custom',
+                        initeditor: function (row, cellvalue, editor, celltext, pressedkey) {
+                            editor.html('<div style="margin: 0px;width: 100%;height: 100%;padding: 5px;"><input type="text" value="'+cellvalue+'" class="jqxgrid_input float_type_positive"><div>');
+                        },
+                        geteditorvalue: function (row, cellvalue, editor) {
+                            return editor.find('input').val();
+                        },
+                        cellendedit: function (row, column, editor,oldvalue,value)
+                        {
+                            var row_data = $('#system_jqx_container').jqxGrid('getrowdata', row);
+                            var sales_commission_percentage=0;
+                            if(!isNaN(parseFloat(row_data['commission_hom'])))
+                            {
+                                sales_commission_percentage=parseFloat(row_data['commission_hom']);
+                            }
+                            var incentive_percentage=0;
+                            if(!isNaN(parseFloat(row_data['incentive_hom'])))
+                            {
+                                incentive_percentage=parseFloat(row_data['incentive_hom']);
+                            }
+                            calculate_total(row, value, sales_commission_percentage,incentive_percentage);
+                        }
+                    },
+                    {text: 'Commission %', dataField: 'commission_hom',align:'center',cellsalign: 'right',width:'110',cellsrenderer: cellsrenderer,rendered: tooltiprenderer,editable:true,columntype:'custom',
+                        initeditor: function (row, cellvalue, editor, celltext, pressedkey) {
+                            editor.html('<div style="margin: 0px;width: 100%;height: 100%;padding: 5px;"><input type="text" value="'+cellvalue+'" class="jqxgrid_input float_type_positive"><div>');
+                        },
+                        geteditorvalue: function (row, cellvalue, editor) {
+                            return editor.find('input').val();
+                        },
+                        cellendedit: function (row, column, editor,oldvalue,value)
+                        {
+                            var row_data = $('#system_jqx_container').jqxGrid('getrowdata', row);
+                            var tp_hom=0;
+                            if(!isNaN(parseFloat(row_data['tp_hom'])))
+                            {
+                                tp_hom=parseFloat(row_data['tp_hom']);
+                            }
+                            var incentive_percentage=0;
+                            if(!isNaN(parseFloat(row_data['incentive_hom'])))
+                            {
+                                incentive_percentage=parseFloat(row_data['incentive_hom']);
+                            }
+                            calculate_total(row, tp_hom,value,incentive_percentage);
+                        }
+                    },
+                    {text: 'Commission', dataField: 'sales_commission',align:'center',cellsalign: 'right',width:'110',cellsrenderer: cellsrenderer,rendered: tooltiprenderer,editable:false},
+                    {text: 'Incentive %', dataField: 'incentive_hom',align:'center',cellsalign: 'right',width:'110',cellsrenderer: cellsrenderer,rendered: tooltiprenderer,editable:true,columntype:'custom',
+                        initeditor: function (row, cellvalue, editor, celltext, pressedkey) {
+                            editor.html('<div style="margin: 0px;width: 100%;height: 100%;padding: 5px;"><input type="text" value="'+cellvalue+'" class="jqxgrid_input float_type_positive"><div>');
+                        },
+                        geteditorvalue: function (row, cellvalue, editor) {
+                            return editor.find('input').val();
+                        },
+                        cellendedit: function (row, column, editor,oldvalue,value)
+                        {
+                            var row_data = $('#system_jqx_container').jqxGrid('getrowdata', row);
+                            var tp_hom=0;
+                            if(!isNaN(parseFloat(row_data['tp_hom'])))
+                            {
+                                tp_hom=parseFloat(row_data['tp_hom']);
+                            }
+                            var sales_commission_percentage=0;
+                            if(!isNaN(parseFloat(row_data['commission_hom'])))
+                            {
+                                sales_commission_percentage=parseFloat(row_data['commission_hom']);
+                            }
+                            calculate_total(row, tp_hom, sales_commission_percentage,value);
+                        }
+                    },
+                    {text: 'Incentive', dataField: 'incentive',align:'center',cellsalign: 'right',width:'110',cellsrenderer: cellsrenderer,rendered: tooltiprenderer,editable:false},
+                    {text: 'Net Price', dataField: 'net_price',align:'center',cellsalign: 'right',width:'110',cellsrenderer: cellsrenderer,rendered: tooltiprenderer,editable:false}
+
+                ]
+            });
+
+    });
+</script>
